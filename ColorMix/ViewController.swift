a1051fd974132dc29d6aafe520c89898cabfd555
Akshay Mittal <akshay.m@turvo.com>
2021-11-14 00:13:12 +0530

    v1 app done

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -12,8 +12,68 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateColor()
+        colorView.layer.borderWidth = 5
+        colorView.layer.cornerRadius = 20
+        colorView.layer.borderColor =
+           UIColor.black.cgColor
+        updateControls()
     }
 
-
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+        updateControls()
+    }
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateColor()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        greenSwitch.isOn = false
+        redSwitch.isOn = false
+        blueSwitch.isOn = false
+        
+        greenSlider.value = 1
+        redSlider.value = 1
+        blueSlider.value = 1
+        
+        updateColor()
+        updateControls()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if blueSwitch.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        let color = UIColor(red: red, green: green,
+           blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    func updateControls() {
+        greenSlider.isEnabled = greenSwitch.isOn
+        redSlider.isEnabled = redSwitch.isOn
+        blueSlider.isEnabled = greenSwitch.isOn
+    }
 }
-
